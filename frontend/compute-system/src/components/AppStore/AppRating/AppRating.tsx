5f0dfd9d94f8a7f01e9c15928c33ae2b794a01f4
KonradMagiera <k.magiera1@wp.pl>
2020-11-04 21:10:05 +0100

    simplify star generation and css for smaller window

--- frontend/compute-system/src/components/AppStore/AppRating/AppRating.tsx
+++ frontend/compute-system/src/components/AppStore/AppRating/AppRating.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Typography, Grid } from "@material-ui/core";
+import { Typography, Grid, useMediaQuery } from "@material-ui/core";
 import ratingSyles from "./appRatingStyles";
 import { Star, StarBorder, StarHalf } from "@material-ui/icons";
 
@@ -10,29 +10,27 @@ interface AppRatingProps {
 
 const AppRating = (props: AppRatingProps) => {
     const classes = ratingSyles();
+    const matches = useMediaQuery('(min-width:700px)');
 
     var stars = new Array(10);
-    var i = 0;
-    for (i = 0; i < Math.floor(props.rate); i++) {
-        stars[i] = <Star className={classes.stars} />;
-    }
-    if (props.rate - Math.floor(props.rate) > 0) {
-        stars[i++] = <StarHalf className={classes.stars}/>;
-    } else {
-        stars[i++] = <StarBorder className={classes.stars}/>;
-    }
-    for (i; i < 10; i++) {
-        stars[i] = <StarBorder className={classes.stars}/>;
-    }
 
+    for (var i = 0; i < 10; i++) {
+        if (i < Math.floor(props.rate)) {
+            stars[i] = <Star className={classes.stars} />;
+        } else if (props.rate - Math.floor(props.rate) > 0 && i < props.rate) {
+            stars[i] = <StarHalf className={classes.stars} />;
+        } else {
+            stars[i] = <StarBorder className={classes.stars} />;
+        }
+    }
 
     return (
         <Grid container alignItems="center" className={classes.grid}>
             <Grid item container alignItems="center" className={classes.header}>
-                <Grid item xs={1}>
+                <Grid item xs={matches ? 1 : 2}>
                     <Typography variant="h6" className={classes.rateValue}>{props.rate}/10</Typography>
                 </Grid>
-                <Grid item xs={10}>
+                <Grid item>
                     {stars}
                 </Grid>
             </Grid>
