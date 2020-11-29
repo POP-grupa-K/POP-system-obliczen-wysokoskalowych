2f1e6c2f5d7a66e2181e861057a3e196080a5704
Łukasz Kamiński <Lukasz.Kaminski@empik.com>
2020-11-29 14:13:05 +0100

    Change way of cunstructing cockpit URL

--- frontend/compute-system/src/api/urls.ts
+++ frontend/compute-system/src/api/urls.ts
@@ -2,7 +2,7 @@ const APPSTORE_BASE_URL = "http://localhost:8005";
 export const APPSTORE_URL = `${APPSTORE_BASE_URL}/appstore/`;
 
 export const COCKPIT_BASE_URL = "http://localhost:8006";
-export const COCKPIT_URL = COCKPIT_BASE_URL + '/cockpit'
+export const COCKPIT_URL = `${COCKPIT_BASE_URL}/cockpit`;
 // TODO
 export const DATASHELF_URL = "http://localhost:TODO";
 
